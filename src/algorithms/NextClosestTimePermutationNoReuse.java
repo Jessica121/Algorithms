6d5111afda8998e44f3d367ed922b562adba489c
Jessica121 <janesis@hotmail.com>
2018-03-03 15:20:32 -0500

    Next Closet Time Added Annotations

--- src/algorithms/NextClosestTimePermutationNoReuse.java
+++ src/algorithms/NextClosestTimePermutationNoReuse.java
@@ -4,24 +4,27 @@ import java.util.*;
 public class NextClosestTimePermutationNoReuse {
 	// Next closest time with permutation only. Cannot reuse digits.
 	    public static void main(String[] args) {
-	        System.out.println(next("11:00"));
+	        System.out.println(next("23:55"));
 	    }
 	    
 	    private static String next(String s) {
-	        int[] arr = {s.charAt(0) - '0', s.charAt(1) - '0',
-	                     s.charAt(3) - '0', s.charAt(4) - '0'};
-	        int tar = arr[0]*1000 + arr[1]*100 +arr[2]*10 +arr[3];
+	        int[] arr = {s.charAt(0) - '0', s.charAt(1) - '0', s.charAt(3) - '0', s.charAt(4) - '0'};
+	        // Target original to find.
+	        int target = arr[0] * 1000 + arr[1] * 100 + arr[2] * 10 + arr[3];
 	        Arrays.sort(arr);
 	        List<Integer> res = new ArrayList<>();
 	        permutate(arr, 0, new HashSet<Integer>(), res); // unique
+	        // Sort permutations increasingly.
 	        Collections.sort(res, (a, b) -> (a - b));
-	        int ind = BS(res, tar);
+	        // Find target, use next. When target being the last, use the first one.
+	        int ind = binarySearch(res, target);
 	        String iRes = res.get((ind + 1) % res.size()).toString();
+	        // Formatting to leading 0s when string length < 4
 	        String resStr = ("0000" + iRes).substring(iRes.length());
 	        return resStr.substring(0, 2) + ":" + resStr.substring(2, 4);
 	    }
 	    
-	    private static int BS(List<Integer> res, int tar) {
+	    private static int binarySearch(List<Integer> res, int tar) {
 	        int s = 0, e = res.size() - 1, mid = 0;
 	        while(s < e) {
 	            mid = s + (e - s) / 2;
@@ -34,6 +37,7 @@ public class NextClosestTimePermutationNoReuse {
 	    
 	    private static void permutate(int[] arr, int sum, Set<Integer> set, List<Integer> res) {
 	        if(set.size() == 4) {
+	        	// If invalid time skip; else add
 	            if(sum / 100 < 24 && sum % 100 < 60) res.add(sum);
 	            return;
 	        }
@@ -43,7 +47,7 @@ public class NextClosestTimePermutationNoReuse {
 	                set.add(i);
 	                sum = sum * 10 + arr[i];
 	                permutate(arr, sum, set, res);
-	                sum = (sum - arr[i])/10;
+	                sum = (sum - arr[i]) / 10;
 	                set.remove(i);
 	            }
 	        }
