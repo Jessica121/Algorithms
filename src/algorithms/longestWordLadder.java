295ec009b70ff2110ea4daefb848c6d617a9036a
Jessica121 <janesis@hotmail.com>
2018-03-09 12:08:53 -0500

    longest word ladder iterative

--- src/algorithms/longestWordLadder.java
+++ src/algorithms/longestWordLadder.java
@@ -5,30 +5,54 @@ public class longestWordLadder {
 	public static void main(String[] args) {
 		Set<String> dic = new HashSet<>();
 		// chat, hat, chats, bat.
-//		dic.add("chat");
+		dic.add("chat");
 		dic.add("chats");
 		dic.add("hat");
 		dic.add("bat");
-		longWL("at", dic, 1);
-		System.out.println(res);
+		
+//		longWLRecursive("at", dic, 1);
+//		System.out.println(res);
+		System.out.println(longWLIter("at", dic));
 	}
 	
-	private static void longWL(String start, Set<String> dic, int len) {
+	private static void longWLRecursive(String start, Set<String> dic, int len) {
 		res = Math.max(res, len);
 		System.out.println(start);
 		StringBuilder sb = new StringBuilder(start);
 		for(char c = 'a'; c <= 'z'; c++) {
 			sb.insert(0, c);
 			if(dic.contains(sb.toString())) {
-				longWL(sb.toString(), dic, len + 1);
+				longWLRecursive(sb.toString(), dic, len + 1);
 			}
 			sb.deleteCharAt(0);
 			sb.append(c);
 			if(dic.contains(sb.toString())) {
-				longWL(sb.toString(), dic, len + 1);
+				longWLRecursive(sb.toString(), dic, len + 1);
 			}
 			sb.deleteCharAt(sb.length() - 1);
 		}
 	}
 
+	private static int longWLIter(String start, Set<String> dic) {
+		int max = 0;
+		Queue<String> que = new LinkedList<>();
+		que.offer(start);
+		int size = 0;
+		while(!que.isEmpty()) {
+			size = que.size();
+			max++;
+			for(int i = 0; i < size; i++) {
+				StringBuilder sb = new StringBuilder(que.poll());
+				for(char c = 'a'; c <= 'z'; c++) {
+					sb.insert(0, c);
+					if(dic.contains(sb.toString())) que.offer(sb.toString());
+					sb.deleteCharAt(0);
+					sb.append(c);
+					if(dic.contains(sb.toString())) que.offer(sb.toString());
+					sb.deleteCharAt(sb.length() - 1);
+				}
+			}
+		}
+		return max;
+	}
 }
